a93a051ccdecd3eff75d31d35a9bd4c03474a23f
Martin Jacobsen <tincho.jacobsen@gmail.com>
2023-01-21 21:55:43 -0300

    added middleware validations to /createuser endpoint and created a custom middleware 'validateFields'

--- controllers/users.js
+++ controllers/users.js
@@ -16,20 +16,21 @@ const getUser =  (req, res) => {
 
 const createUser = async (req = request, res = response) => {
 
+
     const {nombre, correo, password, rol} = req.body
     const usuario = new Usuario({nombre, correo, password, rol});
 
     // Validar si usuario con email dado ya existe
-    const existeUsuario = await Usuario.findOne(usuario);
+    const existeUsuario = await Usuario.findOne({correo});
 
     if (existeUsuario) {
         return res.status(400).json({
-            msg: "Ya existe un usuario con este correo"
+            msg: "Ya existe un usuario con este correo, por favor intenta con otro."
         })
     }
 
     // Encriptar contraseña
-    const salt = bcryptjs.genSalt();
+    const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
     
     // Guardar usuario en BD
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,23 @@
+const { check } = require('express-validator');
+
 const { Router } = require('express');
 const { getUser,
         createUser,
         modifyUser,
         deleteUser } = require('../controllers/users');
+const validarCampos = require('../middlewares/validateFields');
 
 const router = Router();
 
 router.get('/', getUser);
-router.post('/createUser', createUser);
+router.post('/createUser',[
+    check('nombre','El nombre es un campo obligatorio').notEmpty(),
+    check('correo','El correo no es válido').isEmail(),
+    check('password','El campo password es obligatorio y debe tener al menos 6 caracteres').notEmpty().isLength({min: 6}),
+    check('rol','No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos
+
+],createUser);
 router.patch('/:id', modifyUser);
 router.delete('/', deleteUser);
 
